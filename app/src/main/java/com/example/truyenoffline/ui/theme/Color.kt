d0cf55562887fa29555b68e82aa4ed344147b223
cuocsongvanchuong <nguyenquynhduc.business@gmail.com>
2025-12-30 17:02:41 +0700

    Create Color.kt

---
+++ app/src/main/java/com/example/truyenoffline/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.truyenoffline.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+// Màu lấy từ Blogger của bạn
+val PrimaryYellow = Color(0xFFFBBF24) // Màu Vàng chủ đạo
+val TextBlack = Color(0xFF1F2937)     // Màu chữ đen dịu
+val BgWhite = Color(0xFFFFFFFF)       // Nền trắng
